0a913c914dd15b91719f2ed8e64e9d228e613177
exklamationmark <tonghuukhiem@gmail.com>
2026-02-02 23:58:01 -0800

    feat: Initial Bevy app
    Create a very simple Bevy app with:
    - game: The actual binary that runs with Bevy.
    - simulation: All the logic related to Have a sip's gameplay.
    
    Also added a Justfile with some basic tasks.

---
+++ game/src/main.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    simulation::print_copyright();
+    commands.spawn(Camera2d);
+}
