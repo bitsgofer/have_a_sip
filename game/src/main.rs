79d083344ac0588cdb2cbfd58e2ab7b7bb01dabc
exklamationmark <tonghuukhiem@gmail.com>
2026-02-10 00:49:12 -0800

    feat: Create a game window
    - Set explicit log level => can change later.
    - Use 320x180 resolution => This scales 6x to 1080p,
      8x to 1440p and 12x to 4K screens.
      This should cover most screen sizes, based on Steam's hardware survey:
      https://store.steampowered.com/hwsurvey/Steam-Hardware-Software-Survey-Welcome-to-Steam

--- game/src/main.rs
+++ game/src/main.rs
@@ -2,12 +2,24 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(bevy::log::LogPlugin{
+            level: bevy::log::Level::INFO,
+            ..default()
+        }))
         .add_systems(Startup, setup)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     simulation::print_copyright();
-    commands.spawn(Camera2d);
+    commands.spawn((
+        Camera2d,
+        Projection::Orthographic(OrthographicProjection {
+            scaling_mode: bevy::camera::ScalingMode::Fixed {
+                width: 320.0,
+                height: 180.0,
+            },
+            ..OrthographicProjection::default_2d()
+        }),
+    ));
 }
